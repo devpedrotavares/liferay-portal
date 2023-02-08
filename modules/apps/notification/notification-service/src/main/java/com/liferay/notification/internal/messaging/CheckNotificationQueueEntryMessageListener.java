cf89cfd5cb0a149f2a3d00642121f0aab0e64914
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-02-08 14:34:00 -0300

    LPS-175121 Change unsentNotification method to resendNotification method passing status failed

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/messaging/CheckNotificationQueueEntryMessageListener.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/messaging/CheckNotificationQueueEntryMessageListener.java
@@ -15,6 +15,7 @@
 package com.liferay.notification.internal.messaging;
 
 import com.liferay.notification.constants.NotificationConstants;
+import com.liferay.notification.constants.NotificationQueueEntryConstants;
 import com.liferay.notification.internal.configuration.NotificationQueueConfiguration;
 import com.liferay.notification.service.NotificationQueueEntryLocalService;
 import com.liferay.notification.type.NotificationType;
@@ -80,7 +81,9 @@ public class CheckNotificationQueueEntryMessageListener
 			_notificationTypeServiceTracker.getNotificationType(
 				NotificationConstants.TYPE_EMAIL);
 
-		notificationType.sendUnsentNotifications();
+		notificationType.resendNotifications(
+			NotificationQueueEntryConstants.STATUS_FAILED,
+			NotificationConstants.TYPE_EMAIL);
 
 		long deleteInterval =
 			_notificationQueueConfiguration.deleteInterval() * Time.MINUTE;
