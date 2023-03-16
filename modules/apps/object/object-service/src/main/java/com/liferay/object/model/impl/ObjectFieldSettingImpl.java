460190d9e54e5e23fb5a3d8b8b405d68cd84faa3
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-03-16 13:16:49 -0300

    LPS-178344 Add compareName in ObjectFieldSettingImpl

--- modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldSettingImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldSettingImpl.java
@@ -18,11 +18,17 @@ import com.liferay.object.model.ObjectFilter;
 import com.liferay.object.model.ObjectStateFlow;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Marco Leo
  */
 public class ObjectFieldSettingImpl extends ObjectFieldSettingBaseImpl {
+
+	@Override
+	public boolean compareName(String value) {
+		return Objects.equals(getName(), value);
+	}
 
 	public List<ObjectFilter> getObjectFilters() {
 		return _objectFilters;
