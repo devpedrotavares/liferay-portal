4e564f4703f9da46d875f980ca6fbd075cf46920
Pedro Tavares <pedro.tavares@liferay.com>
2023-05-26 16:57:15 -0300

    LPS-185076 Refactor method to validate all multiSelectPicklist keys instead of just the first one

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -113,6 +113,7 @@ import com.liferay.portal.kernel.encryptor.Encryptor;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -204,6 +205,7 @@ import java.sql.Types;
 import java.time.LocalDateTime;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -2045,6 +2047,26 @@ public class ObjectEntryLocalServiceImpl
 		throw new IllegalArgumentException("Invalid function " + function);
 	}
 
+	private Iterable<Object> _getIterable(Object value) {
+		if (value instanceof Iterable) {
+			return (Iterable)value;
+		}
+
+		try {
+			return _jsonFactory.createJSONArray(String.valueOf(value));
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonException);
+			}
+
+			return Arrays.asList(
+				StringUtil.split(
+					GetterUtil.getString(String.valueOf(value)),
+					StringPool.COMMA_AND_SPACE));
+		}
+	}
+
 	private Key _getKey() throws PortalException {
 		return new SecretKeySpec(
 			Base64.decode(PropsValues.OBJECT_ENCRYPTION_KEY),
@@ -3870,6 +3892,22 @@ public class ObjectEntryLocalServiceImpl
 		}
 	}
 
+	private void _validateListTypeEntryKey(
+			Map.Entry<String, Serializable> entry, String listTypeEntryKey,
+			ObjectField objectField)
+		throws PortalException {
+
+		ListTypeEntry listTypeEntry =
+			_listTypeEntryLocalService.fetchListTypeEntry(
+				objectField.getListTypeDefinitionId(), listTypeEntryKey);
+
+		if ((listTypeEntry == null) &&
+			(!listTypeEntryKey.isEmpty() || objectField.isRequired())) {
+
+			throw new ObjectEntryValuesException.ListTypeEntry(entry.getKey());
+		}
+	}
+
 	private void _validateObjectStateTransition(
 			Map.Entry<String, Serializable> entry, long listTypeDefinitionId,
 			ObjectEntry objectEntry, long objectFieldId, long userId)
@@ -4288,30 +4326,27 @@ public class ObjectEntryLocalServiceImpl
 		}
 
 		if (objectField.getListTypeDefinitionId() != 0) {
-			ListTypeEntry listTypeEntry = null;
+			if (objectField.compareBusinessType(
+					ObjectFieldConstants.BUSINESS_TYPE_MULTISELECT_PICKLIST)) {
 
-			String value = String.valueOf(values.get(entry.getKey()));
-
-			for (ListTypeEntry curListTypeEntry :
-					_listTypeEntryLocalService.getListTypeEntries(
-						objectField.getListTypeDefinitionId())) {
-
-				if (Objects.equals(value, curListTypeEntry.getKey())) {
-					listTypeEntry = curListTypeEntry;
-
-					break;
+				for (Object listTypeEntryKey : _getIterable(entry.getValue())) {
+					_validateListTypeEntryKey(
+						entry,
+						GetterUtil.getString(String.valueOf(listTypeEntryKey)),
+						objectField);
 				}
 			}
+			else {
+				_validateListTypeEntryKey(
+					entry,
+					GetterUtil.getString(String.valueOf(entry.getValue())),
+					objectField);
 
-			if ((listTypeEntry == null) && objectField.isRequired()) {
-				throw new ObjectEntryValuesException.ListTypeEntry(
-					entry.getKey());
-			}
-
-			if ((objectEntry != null) && objectField.isState()) {
-				_validateObjectStateTransition(
-					entry, objectField.getListTypeDefinitionId(), objectEntry,
-					objectField.getObjectFieldId(), userId);
+				if ((objectEntry != null) && objectField.isState()) {
+					_validateObjectStateTransition(
+						entry, objectField.getListTypeDefinitionId(),
+						objectEntry, objectField.getObjectFieldId(), userId);
+				}
 			}
 		}
 	}
