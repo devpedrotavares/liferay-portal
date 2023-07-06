0eead7319791068f92064f5c45cde6d6e0dea3e9
Pedro Tavares <pedro.tavares@liferay.com>
2023-07-06 18:10:08 -0300

    buildService

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -173,7 +173,7 @@ public class ObjectDefinitionLocalServiceImpl
 			long userId, boolean enableComments, boolean enableLocalization,
 			Map<Locale, String> labelMap, String name, String panelAppOrder,
 			String panelCategoryKey, Map<Locale, String> pluralLabelMap,
-			boolean portlet, String scope, String storageType,
+			boolean portlet, boolean showPanel, String scope, String storageType,
 			List<ObjectField> objectFields)
 		throws PortalException {
 
@@ -1005,6 +1005,7 @@ public class ObjectDefinitionLocalServiceImpl
 		objectDefinition.setSystem(system);
 		objectDefinition.setVersion(version);
 		objectDefinition.setStatus(status);
+		objectDefinition.setShowPanelApp(!objectDefinition.getName().endsWith("hidden"));
 
 		objectDefinition = objectDefinitionPersistence.update(objectDefinition);
 
--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionServiceImpl.java
@@ -52,7 +52,7 @@ public class ObjectDefinitionServiceImpl
 			boolean enableComments, boolean enableLocalization,
 			Map<Locale, String> labelMap, String name, String panelAppOrder,
 			String panelCategoryKey, Map<Locale, String> pluralLabelMap,
-			boolean portlet, String scope, String storageType,
+			boolean portlet, String scope, boolean showPanel, String storageType,
 			List<ObjectField> objectFields)
 		throws PortalException {
 
@@ -62,7 +62,7 @@ public class ObjectDefinitionServiceImpl
 
 		return objectDefinitionLocalService.addCustomObjectDefinition(
 			getUserId(), enableComments, enableLocalization, labelMap, name,
-			panelAppOrder, panelCategoryKey, pluralLabelMap, portlet, scope,
+			panelAppOrder, panelCategoryKey, pluralLabelMap, portlet, showPanel, scope,
 			storageType, objectFields);
 	}
 
