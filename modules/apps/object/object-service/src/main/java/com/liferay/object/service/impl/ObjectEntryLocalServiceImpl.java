c2c67713960f52fc31dc69c3faf199f3810c5a6f
Pedro Tavares <pedro.tavares@liferay.com>
2023-08-17 18:59:13 -0300

    LPS-193817 Skip validation for required fields when saving object entry as draft

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -253,7 +253,8 @@ public class ObjectEntryLocalServiceImpl
 
 		_validateValues(
 			user.isGuestUser(), objectDefinitionId, null,
-			objectDefinition.getPortletId(), serviceContext, userId, values);
+			objectDefinition.getPortletId(), serviceContext, status, userId,
+			values);
 
 		long objectEntryId = counterLocalService.increment();
 
@@ -339,7 +340,8 @@ public class ObjectEntryLocalServiceImpl
 
 		_validateValues(
 			user.isGuestUser(), objectDefinition.getObjectDefinitionId(), null,
-			objectDefinition.getClassName(), serviceContext, userId, values);
+			objectDefinition.getClassName(), serviceContext,
+			WorkflowConstants.STATUS_APPROVED, userId, values);
 
 		insertIntoOrUpdateExtensionTable(
 			userId, objectDefinition.getObjectDefinitionId(), primaryKey,
@@ -1364,7 +1366,7 @@ public class ObjectEntryLocalServiceImpl
 		_validateValues(
 			user.isGuestUser(), objectEntry.getObjectDefinitionId(),
 			objectEntry, objectDefinition.getPortletId(), serviceContext,
-			userId, values);
+			status, userId, values);
 
 		Map<String, Serializable> transientValues = objectEntry.getValues();
 
@@ -4069,8 +4071,8 @@ public class ObjectEntryLocalServiceImpl
 
 	private void _validateValues(
 			boolean guestUser, long objectDefinitionId, ObjectEntry objectEntry,
-			String portletId, ServiceContext serviceContext, long userId,
-			Map<String, Serializable> values)
+			String portletId, ServiceContext serviceContext, int status,
+			long userId, Map<String, Serializable> values)
 		throws PortalException {
 
 		List<ObjectField> objectFields =
@@ -4107,14 +4109,14 @@ public class ObjectEntryLocalServiceImpl
 		for (Map.Entry<String, Serializable> entry : values.entrySet()) {
 			_validateValues(
 				guestUser, entry, objectDefinitionId, objectEntry, portletId,
-				serviceContext, userId, values);
+				serviceContext, status, userId, values);
 		}
 	}
 
 	private void _validateValues(
 			boolean guestUser, Map.Entry<String, Serializable> entry,
 			long objectDefinitionId, ObjectEntry objectEntry, String portletId,
-			ServiceContext serviceContext, long userId,
+			ServiceContext serviceContext, int status, long userId,
 			Map<String, Serializable> values)
 		throws PortalException {
 
@@ -4135,8 +4137,12 @@ public class ObjectEntryLocalServiceImpl
 		if (Validator.isNull(values.get(objectField.getName())) &&
 			objectField.isRequired()) {
 
-			throw new ObjectEntryValuesException.Required(
-				objectField.getName());
+			if (status == WorkflowConstants.STATUS_APPROVED) {
+				throw new ObjectEntryValuesException.Required(
+					objectField.getName());
+			}
+
+			return;
 		}
 		else if (StringUtil.equals(
 					objectField.getBusinessType(),
