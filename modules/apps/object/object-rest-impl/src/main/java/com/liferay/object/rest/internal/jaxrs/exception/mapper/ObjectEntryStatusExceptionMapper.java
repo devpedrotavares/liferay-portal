a7e40eda750666d9ff6678c76421baf24d5a5ef5
Pedro Tavares <pedro.tavares@liferay.com>
2023-08-17 18:25:21 -0300

    LPS-193816 Create ObjectEntryStatusException and ExceptionMapper

---
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectEntryStatusExceptionMapper.java
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectEntryStatusException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Pedro Tavares
+ */
+@Provider
+public class ObjectEntryStatusExceptionMapper
+	extends BaseExceptionMapper<ObjectEntryStatusException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectEntryStatusException objectEntryStatusException) {
+
+		return new Problem(objectEntryStatusException);
+	}
+
+}
