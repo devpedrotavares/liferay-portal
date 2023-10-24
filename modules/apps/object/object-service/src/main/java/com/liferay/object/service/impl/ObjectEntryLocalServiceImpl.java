b650efe8e06f91c6641c4290aeec4e1e8400067b
Pedro Tavares <pedro.tavares@liferay.com>
2023-10-24 17:02:25 -0300

    LPS-173502 DRY and remove Validator.isNull

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3019,12 +3019,22 @@ public class ObjectEntryLocalServiceImpl
 	 * @see com.liferay.portal.upgrade.util.Table#getValue
 	 */
 	private Object _getValue(Object object, int sqlType) throws SQLException {
-		if (sqlType == Types.BIGINT) {
+		if (_isNumeric(sqlType)) {
 			if (object == null) {
 				return null;
 			}
-
-			return GetterUtil.getLong(object);
+			else if (sqlType == Types.BIGINT) {
+				return GetterUtil.getLong(object);
+			}
+			else if (sqlType == Types.DECIMAL) {
+				return object;
+			}
+			else if (sqlType == Types.DOUBLE) {
+				return GetterUtil.getDouble(object);
+			}
+			else if (sqlType == Types.INTEGER) {
+				return GetterUtil.getInteger(object);
+			}
 		}
 		else if (sqlType == Types.BOOLEAN) {
 			return GetterUtil.getBoolean(object);
@@ -3040,23 +3050,6 @@ public class ObjectEntryLocalServiceImpl
 			Date date = (Date)object;
 
 			return new Timestamp(date.getTime());
-		}
-		else if (sqlType == Types.DECIMAL) {
-			return object;
-		}
-		else if (sqlType == Types.DOUBLE) {
-			if (object == null) {
-				return null;
-			}
-
-			return GetterUtil.getDouble(object);
-		}
-		else if (sqlType == Types.INTEGER) {
-			if (object == null) {
-				return null;
-			}
-
-			return GetterUtil.getInteger(object);
 		}
 		else if (sqlType == Types.VARCHAR) {
 			return object;
@@ -3423,6 +3416,16 @@ public class ObjectEntryLocalServiceImpl
 		}
 	}
 
+	private boolean _isNumeric(int sqlType) {
+		if ((sqlType == Types.BIGINT) || (sqlType == Types.DECIMAL) ||
+			(sqlType == Types.DOUBLE) || (sqlType == Types.INTEGER)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 	private List<Object[]> _list(
 			DSLQuery dslQuery, long objectDefinitionId,
 			Expression<?>[] selectExpressions)
@@ -3644,12 +3647,26 @@ public class ObjectEntryLocalServiceImpl
 			Object value)
 		throws Exception {
 
-		if (sqlType == Types.BIGINT) {
-			if (Validator.isNull(value)) {
-				preparedStatement.setNull(index, Types.BIGINT);
+		if (_isNumeric(sqlType)) {
+			if ((value == null) || StringPool.BLANK.equals(value)) {
+				preparedStatement.setNull(index, sqlType);
+			}
+			else if (sqlType == Types.BIGINT) {
+				preparedStatement.setLong(index, GetterUtil.getLong(value));
+			}
+			else if (sqlType == Types.DECIMAL) {
+				preparedStatement.setBigDecimal(
+					index,
+					new BigDecimal(_toPeriodSeparator(String.valueOf(value))));
+			}
+			else if (sqlType == Types.DOUBLE) {
+				preparedStatement.setDouble(
+					index,
+					GetterUtil.getDouble(
+						_toPeriodSeparator(String.valueOf(value))));
 			}
 			else {
-				preparedStatement.setLong(index, GetterUtil.getLong(value));
+				preparedStatement.setInt(index, GetterUtil.getInteger(value));
 			}
 		}
 		else if (sqlType == Types.BLOB) {
@@ -3697,35 +3714,6 @@ public class ObjectEntryLocalServiceImpl
 
 				preparedStatement.setTimestamp(
 					index, new Timestamp(date.getTime()));
-			}
-		}
-		else if (sqlType == Types.DECIMAL) {
-			if (Validator.isNull(String.valueOf(value))) {
-				preparedStatement.setNull(index, Types.DECIMAL);
-			}
-			else {
-				preparedStatement.setBigDecimal(
-					index,
-					new BigDecimal(_toPeriodSeparator(String.valueOf(value))));
-			}
-		}
-		else if (sqlType == Types.DOUBLE) {
-			if (Validator.isNull(value)) {
-				preparedStatement.setNull(index, Types.DOUBLE);
-			}
-			else {
-				preparedStatement.setDouble(
-					index,
-					GetterUtil.getDouble(
-						_toPeriodSeparator(String.valueOf(value))));
-			}
-		}
-		else if (sqlType == Types.INTEGER) {
-			if (Validator.isNull(value)) {
-				preparedStatement.setNull(index, Types.INTEGER);
-			}
-			else {
-				preparedStatement.setInt(index, GetterUtil.getInteger(value));
 			}
 		}
 		else if (sqlType == Types.VARCHAR) {
