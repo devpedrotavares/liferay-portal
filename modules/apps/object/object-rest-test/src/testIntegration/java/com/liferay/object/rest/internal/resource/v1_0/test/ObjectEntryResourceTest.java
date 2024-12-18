64793259ac8d6a553ef4e342ffb222e20285e627
Pedro Tavares <pedro.tavares@liferay.com>
2024-12-18 16:08:38 -0300

    LPD-35351 Add test for localized attachment field with existing and nonexistent ERC

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -38,6 +38,7 @@ import com.liferay.object.constants.ObjectFieldValidationConstants;
 import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.constants.ObjectValidationRuleConstants;
 import com.liferay.object.constants.ObjectValidationRuleSettingConstants;
+import com.liferay.object.field.builder.AttachmentObjectFieldBuilder;
 import com.liferay.object.field.builder.LongTextObjectFieldBuilder;
 import com.liferay.object.field.builder.RichTextObjectFieldBuilder;
 import com.liferay.object.field.builder.TextObjectFieldBuilder;
@@ -214,7 +215,7 @@ import org.springframework.transaction.support.DefaultTransactionStatus;
 /**
  * @author Luis Miguel Barcos
  */
-@FeatureFlags("LPS-164801")
+@FeatureFlags({"LPD-32050", "LPS-164801"})
 @RunWith(Arquillian.class)
 public class ObjectEntryResourceTest {
 
@@ -8996,6 +8997,159 @@ public class ObjectEntryResourceTest {
 
 		_testPutCustomObjectEntryUnlinkNestedCustomObjectEntriesByExternalReferenceCode(
 			false);
+	}
+
+	@Test
+	public void testPutCustomObjectEntryWithLocalizedAttachmentObjectField()
+		throws Exception {
+
+		// File with a nonexistent external reference code
+
+		String objectDefinitionName = ObjectDefinitionTestUtil.getRandomName();
+
+		ObjectDefinition objectDefinition =
+			ObjectDefinitionTestUtil.publishObjectDefinition(
+				true, objectDefinitionName,
+				Collections.singletonList(
+					new AttachmentObjectFieldBuilder(
+					).labelMap(
+						LocalizedMapUtil.getLocalizedMap("localizedAttachment")
+					).localized(
+						true
+					).name(
+						"localizedAttachment"
+					).objectFieldSettings(
+						Arrays.asList(
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.
+									NAME_ACCEPTED_FILE_EXTENSIONS
+							).value(
+								"txt"
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.NAME_FILE_SOURCE
+							).value(
+								ObjectFieldSettingConstants.VALUE_USER_COMPUTER
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.NAME_MAX_FILE_SIZE
+							).value(
+								"100"
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.
+									NAME_SHOW_FILES_IN_DOCS_AND_MEDIA
+							).value(
+								StringPool.TRUE
+							).build(),
+							new ObjectFieldSettingBuilder(
+							).name(
+								ObjectFieldSettingConstants.
+									NAME_STORAGE_DL_FOLDER_PATH
+							).value(
+								StringPool.SLASH + objectDefinitionName
+							).build())
+					).build()),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				TestPropsValues.getUserId());
+
+		String fileContent = RandomTestUtil.randomString();
+
+		com.liferay.object.rest.dto.v1_0.FileEntry testFileEntry = _toFileEntry(
+			Base64::encode, fileContent, RandomTestUtil.randomString() + ".txt",
+			null, null);
+
+		testFileEntry.setExternalReferenceCode(RandomTestUtil.randomString());
+
+		Group group = GroupTestUtil.addGroup();
+
+		Scope scope = new Scope();
+
+		scope.setExternalReferenceCode(group.getExternalReferenceCode());
+		scope.setType(Scope.Type.SITE);
+
+		testFileEntry.setScope(scope);
+
+		JSONObject jsonObject = HTTPTestUtil.invokeToJSONObject(
+			String.valueOf(_jsonFactory.createJSONObject()),
+			objectDefinition.getRESTContextPath(), Http.Method.POST);
+
+		String endpoint = StringBundler.concat(
+			objectDefinition.getRESTContextPath(), StringPool.SLASH,
+			jsonObject.get("id"));
+
+		jsonObject = HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"localizedAttachment_i18n",
+				JSONUtil.put(
+					"en_US",
+					JSONFactoryUtil.createJSONObject(testFileEntry.toString()))
+			).toString(),
+			endpoint + "?nestedFields=localizedAttachment.fileBase64",
+			Http.Method.PUT);
+
+		DLFileEntry dlFileEntry = _dlFileEntryLocalService.getFileEntry(
+			_testDLFileEntryModelListener.getLastFileEntryId());
+
+		JSONObject localizedValuesJSONObject = jsonObject.getJSONObject(
+			"localizedAttachment_i18n");
+
+		_assertAttachmentJSONObject(
+			dlFileEntry, Base64.encode(fileContent.getBytes()),
+			localizedValuesJSONObject.getJSONObject("en_US"),
+			_jsonFactory.createJSONObject(scope.toString()));
+
+		// File with an existing external reference code
+
+		com.liferay.portal.kernel.repository.model.Folder existingFolder =
+			_dlAppLocalService.addFolder(
+				null, TestPropsValues.getUserId(), _group.getGroupId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				ServiceContextTestUtil.getServiceContext());
+
+		DLFileEntry existingFile = _addDLFileEntry(
+			StringPool.BLANK, existingFolder.getFolderId());
+
+		testFileEntry = _toFileEntry(
+			content -> null, StringPool.BLANK, existingFile.getFileName(), null,
+			TestPropsValues.getGroupId());
+
+		testFileEntry.setExternalReferenceCode(
+			existingFile.getExternalReferenceCode());
+
+		jsonObject = HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"localizedAttachment_i18n",
+				JSONUtil.put(
+					"en_US",
+					JSONFactoryUtil.createJSONObject(testFileEntry.toString())
+				).put(
+					"pt_BR",
+					JSONFactoryUtil.createJSONObject(testFileEntry.toString())
+				)
+			).toString(),
+			endpoint, Http.Method.PUT);
+
+		localizedValuesJSONObject = jsonObject.getJSONObject(
+			"localizedAttachment_i18n");
+
+		JSONObject fileEntryJSONObject =
+			localizedValuesJSONObject.getJSONObject("en_US");
+
+		Assert.assertEquals(
+			testFileEntry.getExternalReferenceCode(),
+			fileEntryJSONObject.getString("externalReferenceCode"));
+
+		fileEntryJSONObject = localizedValuesJSONObject.getJSONObject("pt_BR");
+
+		Assert.assertEquals(
+			testFileEntry.getExternalReferenceCode(),
+			fileEntryJSONObject.getString("externalReferenceCode"));
 	}
 
 	@Test
